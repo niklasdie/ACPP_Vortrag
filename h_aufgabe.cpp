2388a0d1af0c844069f9297de12ff43351b086f9
niklas <MHLimM1414>
2022-05-29 20:55:55 +0200

    update

--- h_aufgabe.cpp
+++ h_aufgabe.cpp
@@ -1,7 +1,7 @@
 #include <memory>
 #include <iostream>
 
-enum Marken {
+enum Automarke {
     VW, Porsche
 };
 
@@ -11,36 +11,63 @@ struct Reifen
     int hoehe;
     int zoll;
 
-    Reifen(int breite, int hoehe, int zoll): breite{breite}, hoehe{hoehe}, zoll{zoll} {}
-    Reifen(const Reifen& reifen1): hoehe{reifen1.hoehe}, breite{reifen1.breite}, zoll{reifen1.zoll} {}
+    Reifen(int breite, int hoehe, int zoll): breite{breite}, hoehe{hoehe}, zoll{zoll} { }
+    Reifen(const Reifen& reifen1): hoehe{reifen1.hoehe}, breite{reifen1.breite}, zoll{reifen1.zoll} { }
 };
 
 struct Car
 {
-    Marken marke;
-    std::string model;
+    Automarke marke;
+    std::string model; // TODO
     int ps;
-    long fin; // Fahrzeug-Identifikationsnummer
-    Reifen reifen;
+    long fin; // Fahrzeug-Identifikationsnummer // TODO
+    Reifen reifen; // TODO
+    std::string besitzer; // TODO
 
-    Car(Marken marke, std::string model, int ps, long fin, Reifen reifen) :
-            marke{marke}, model{model}, ps{ps}, fin{fin}, reifen{reifen} {}
+    Car(Automarke marke, std::string model, int ps, long fin, Reifen reifen) :
+            marke{marke}, model{model}, ps{ps}, fin{fin}, reifen{reifen} { } // TODO
+
+    void set_besitzer(std::string besitzer) { // TODO
+        this->besitzer = besitzer;
+    }
+
+    [[nodiscard]] std::string get_besitzer() const { // TODO
+        return besitzer;
+    }
 };
 
-int reichweite_berechnen(Car car) {
+int reichweite_berechnen(const Car& car) { // TODO
     return 700 - car.ps / 2;    // irgendeine Berechnung
+}
+
+Car reifen_wechseln(Car car, Reifen reifen) { // TODO
+    car.reifen = reifen;
+    return std::move(car);
 }
 
 int main()
 {
-    Reifen reifen_a = *new Reifen(255, 35, 20);
-    Reifen reifen_b = *new Reifen(205, 45, 17);
+    // shared_ptr
+    Reifen reifen_a = *new Reifen(255, 35, 20); // TODO
+    Reifen reifen_b = *new Reifen(205, 45, 17); // TODO
 
-    Car vw_golf = *new Car(Marken::VW, "Golf", 130, 15574685135248658, reifen_b);
-    Car vw_polo = *new Car(Marken::VW, "Polo", 110, 54896123845972584, reifen_b);
-    Car porsche_911 = *new Car(Marken::Porsche, "911", 640, 45861587596328452, reifen_a);
+    // unique_ptr
+    Car vw_golf =
+            *new Car(Automarke::VW, "Golf", 130, 15574685135248658, reifen_b); // TODO
+    Car vw_polo =
+            *new Car(Automarke::VW, "Polo", 110, 54896123845972584, reifen_b); // TODO
+    Car porsche_911 =
+            *new Car(Automarke::Porsche, "911", 640, 45861587596328452, reifen_a); // TODO
 
-    std::cout << "VW Golf Reichweite: " << reichweite_berechnen(vw_golf) << " km\n";
-    std::cout << "VW Golf Reichweite: " << reichweite_berechnen(vw_polo) << " km\n";
-    std::cout << "Porsche 911 Reichweite: " << reichweite_berechnen(porsche_911) << " km\n";
+    std::cout << "VW Golf Reichweite: " << reichweite_berechnen(vw_golf) << " km\n"; // TODO
+    std::cout << "VW Golf Reichweite: " << reichweite_berechnen(vw_polo) << " km\n"; // TODO
+    std::cout << "Porsche 911 Reichweite: " << reichweite_berechnen(porsche_911) << " km\n\n"; // TODO
+
+    vw_golf = reifen_wechseln(vw_golf, reifen_a); // TODO
+    porsche_911 = reifen_wechseln(porsche_911, reifen_b); // TODO
+
+    std::cout << "VW Golf Reifen: " << vw_golf.reifen.breite << " / " << vw_golf.reifen.hoehe // TODO
+              << " " << vw_golf.reifen.zoll << "\n"; // TODO
+    std::cout << "Porsche 911 Reifen: " << porsche_911.reifen.breite << " / " << porsche_911.reifen.hoehe // TODO
+              << " " << porsche_911.reifen.zoll << "\n"; // TODO
 }
--- h_aufgabe_sol.cpp
+++ h_aufgabe_sol.cpp
@@ -1,7 +1,7 @@
 #include <memory>
 #include <iostream>
 
-enum Marken {
+enum Automarke {
     VW, Porsche
 };
 
@@ -11,20 +11,30 @@ struct Reifen
     int hoehe;
     int zoll;
 
-    Reifen(int breite, int hoehe, int zoll): breite{breite}, hoehe{hoehe}, zoll{zoll} {}
-    Reifen(const Reifen& reifen1): hoehe{reifen1.hoehe}, breite{reifen1.breite}, zoll{reifen1.zoll} {}
+    Reifen(int breite, int hoehe, int zoll): breite{breite}, hoehe{hoehe}, zoll{zoll} { }
+    Reifen(const Reifen& reifen1): hoehe{reifen1.hoehe}, breite{reifen1.breite}, zoll{reifen1.zoll} { }
 };
 
 struct Car
 {
-    Marken marke;
+    Automarke marke;
     std::string model;
     int ps;
     std::unique_ptr<long> fin; // Fahrzeug-Identifikationsnummer
     std::shared_ptr<Reifen> reifen;
+    std::unique_ptr<std::string> besitzer;
 
-    Car(Marken marke, std::string model, int ps, long fin, std::shared_ptr<Reifen> reifen) :
-            marke{marke}, model{model}, ps{ps}, fin{std::make_unique<long>(fin)}, reifen{reifen} {}
+    Car(Automarke marke, std::string model, int ps, long fin, std::shared_ptr<Reifen> reifen) :
+            marke{marke}, model{model}, ps{ps}, fin{std::make_unique<long>(fin)}, reifen{reifen} { }
+            // kein std::move(reifen), wir wollen den counter nicht bewegen, sondern erhöhen
+
+    void set_besitzer(std::unique_ptr<std::string> new_besitzer) {
+        besitzer = std::move(new_besitzer); // std::move() mit Ownership
+    }
+
+    [[nodiscard]] std::string get_besitzer() const {
+        return besitzer.operator*(); // ohne std::move() um das Ownership beizubehalten
+    }
 };
 
 int reichweite_berechnen(int ps) {
@@ -32,8 +42,8 @@ int reichweite_berechnen(int ps) {
 }
 
 std::unique_ptr<Car> reifen_wechseln(std::unique_ptr<Car> car, std::shared_ptr<Reifen> reifen) {
-    car->reifen = reifen;
-    return std::move(car);
+    car->reifen = reifen; // hier auch kein std::move(reifen)
+    return std::move(car); // std::move(car) um das Ownership mit zurückzugeben
 }
 
 int main()
@@ -42,15 +52,15 @@ int main()
     auto reifen_b = std::make_shared<Reifen>(205, 45, 17);
 
     auto vw_golf =
-            std::make_unique<Car>(Marken::VW, "Golf", 130, 15574685135248658, reifen_b);
+            std::make_unique<Car>(Automarke::VW, "Golf", 130, 15574685135248658, reifen_b);
     auto vw_polo =
-            std::make_unique<Car>(Marken::VW, "Polo", 110, 54896123845972584, reifen_b);
+            std::make_unique<Car>(Automarke::VW, "Polo", 110, 54896123845972584, reifen_b);
     auto porsche_911 =
-            std::make_unique<Car>(Marken::Porsche, "911", 640, 45861587596328452, reifen_a);
+            std::make_unique<Car>(Automarke::Porsche, "911", 640, 45861587596328452, reifen_a);
 
     std::cout << "VW Golf Reichweite: " << reichweite_berechnen(vw_golf->ps) << " km\n";
     std::cout << "VW Golf Reichweite: " << reichweite_berechnen(vw_polo->ps) << " km\n";
-    std::cout << "Porsche 911 Reichweite: " << reichweite_berechnen(porsche_911->ps) << " km\n";
+    std::cout << "Porsche 911 Reichweite: " << reichweite_berechnen(porsche_911->ps) << " km\n\n";
 
     vw_golf = reifen_wechseln(std::move(vw_golf), reifen_a);
     porsche_911 = reifen_wechseln(std::move(porsche_911), reifen_b);
